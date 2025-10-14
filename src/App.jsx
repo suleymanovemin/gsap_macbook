d69034f34854ebab101ec9f424704e236ac72c20
suleymanovemin <suleymanov.emin2001@gmail.com>
2025-10-14 15:23:11 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const navLinks = [
+  {
+    label: "Store",
+  },
+  {
+    label: "Mac",
+  },
+  {
+    label: "iPhone",
+  },
+  {
+    label: "Watch",
+  },
+  {
+    label: "Vision",
+  },
+  {
+    label: "AirPods",
+  },
+];
